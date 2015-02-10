5f3cc97e371472669bd08e2306d2273ee1d2df00
Marios Kogias <marioskogias@gmail.com>
2015-02-10 14:36:52 +0200

    Add year field in JSONAlchemy

---
+++ invenio_demosite/base/recordext/functions/get_year_from_creation_date.py
@@ -0,0 +1,7 @@
+def get_year_from_creation_date(date):
+    """
+    Extracts the year from the creation date
+    @param date format: 'YYYY-MM-DDTHH:MM:SS'
+    @return  the creation yaer of the record
+    """
+    return date.year
